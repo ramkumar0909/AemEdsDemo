e81ca3241fbf131a503b63f28ab25eebf1f710be
Ramkumar R <r.raamkumar@gmail.com>
2026-03-05 16:53:17 +0000

    removed eventlisteners

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -10,6 +10,7 @@ export default function decorate(block) {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
+    li.append('Test');
     ul.append(li);
   });
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
--- blocks/shopifyCard/shopifyCard.js
+++ blocks/shopifyCard/shopifyCard.js
@@ -161,5 +161,4 @@ export default async function decorate(block) {
   } catch (e) {
     renderState(block, titleText, 'error', `Could not load products. ${e.message}`);
   }
-  addEventListeners(block);
 }
