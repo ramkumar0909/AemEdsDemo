a41f4644b913b91ced31835172b17ae7e193038c
Ramkumar R <r.raamkumar@gmail.com>
2026-03-05 17:02:43 +0000

    pull dynamic data from my local

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,8 +1,22 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
-export default function decorate(block) {
+export default async function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
+
+  // Fetch data from localhost:8080
+  let data = 'Test';
+  try {
+    const response = await fetch('http://localhost:8080/data');
+    if (response.ok) {
+      const fetchedData = await response.json();
+      data = fetchedData;
+    }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error fetching data:', error);
+  }
+
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
@@ -10,7 +24,7 @@ export default function decorate(block) {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
-    li.append('Test');
+    li.append(data);
     ul.append(li);
   });
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
